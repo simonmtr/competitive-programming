467c8b6a3bf17942b9bdfd35d521d6ff6ba1f021
simonmtr <294simon@googlemail.com>
2025-06-23 19:20:02 +0200

    20

---
+++ problems/leetcode/20.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for (char c : s) {
+            if (c == '(' || c == '{' || c == '[') {
+                st.push(c);
+            } else {
+                if (st.empty() || 
+                    (st.top() == '(' && c != ')') ||
+                    (st.top() == '{' && c != '}') || 
+                    (st.top() == '[' && c != ']')
+                    ) {
+                        return false;
+                    }
+                    st.pop();
+            }
+        }
+        return st.empty();
+    }
+};

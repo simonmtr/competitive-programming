146dd28cee03996beae73838151281c6461f380a
simonmtr <294simon@googlemail.com>
2025-11-15 11:23:45 +0100

    143 LC

---
+++ problems/leetcode/143.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    void reorderList(ListNode* head) {
+        ListNode* fast = head;
+        ListNode* slow = head;
+
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        ListNode* second = slow->next;
+        slow->next = nullptr;
+        ListNode* n = nullptr; 
+        while (second) {
+            ListNode* temp = second->next;
+            second->next = n;
+            n = second;
+            second = temp;
+        }
+
+        ListNode* first = head;
+        second = n;
+        while(second) {
+            ListNode* temp1 = first->next;
+            ListNode* temp2 = second->next;
+            first->next = second;
+            second->next = temp1;
+            first = temp1;
+            second = temp2;
+        }
+    }
+};

7e854cadda7f7d596c0697efb6c4bfdae97d3c97
simonmtr <294simon@googlemail.com>
2026-03-29 21:08:59 +0200

    cf 1089

---
+++ problems/contests/codeforces/cf_1089/A.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        
+        cout << n << " ";
+        for (int i = n - 1; i>=1; --i) {
+            cout << i << " ";
+        }
+        cout << endl;
+
+    }
+}

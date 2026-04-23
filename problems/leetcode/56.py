e3fdf6435c5f99d81e3ed1625b6f4df18738c9a7
simonmtr <294simon@googlemail.com>
2026-04-23 21:17:16 +0200

    LC: 56

---
+++ problems/leetcode/56.py
@@ -0,0 +1,14 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        res = []
+        intervals.sort(key=lambda x: x[0])
+        prev = intervals[0]
+
+        for interval in intervals[1:]:
+            if interval[0] <= prev[1]:
+                prev[1] = max(prev[1], interval[1])
+            else:
+                res.append(prev)
+                prev = interval
+        res.append(prev)
+        return res

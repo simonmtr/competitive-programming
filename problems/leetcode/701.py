84829ca8b0c40cc2416554c7d9f5c5c0911cb0e4
simonmtr <294simon@googlemail.com>
2026-04-23 21:06:16 +0200

    LC: 701

---
+++ problems/leetcode/701.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def insertIntoBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        new_node = TreeNode(val)
+        if not root:
+            return new_node
+
+        cur = root
+
+        while cur:
+            if val < cur.val:
+                if cur.left != None:
+                    cur = cur.left
+                else:
+                    cur.left = new_node
+                    break
+            else:
+                if cur.right != None:
+                    cur = cur.right
+                else:
+                    cur.right = new_node
+                    break
+        return root
